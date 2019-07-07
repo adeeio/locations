305d221e46a8f6da19707a5745413cfffdf52007
Adrian Ostapkiewicz <ostapkiewicz.adrian@gmail.com>
2019-07-07 22:10:39 +0100

    flickity progress bar done

--- js/script.js
+++ js/script.js
@@ -14,9 +14,17 @@ var flkty = new Flickity(elem, {
     pageDots: false
 });
 
+var fl = new Flickity('.main-carousel');
 document.querySelector('.restart').addEventListener('click', function(event){
-    var fl = new Flickity('.main-carousel');
+    
     var selector = document.querySelector('.cell1');
     fl.selectCell(selector);
-
 });
+
+
+var progressBar = document.querySelector('.progress-bar');
+
+fl.on('scroll', function(progress) {
+    progress = Math.max(0, Math.min(1, progress));
+    progressBar.style.width = progress * 100 + '%';
+})
